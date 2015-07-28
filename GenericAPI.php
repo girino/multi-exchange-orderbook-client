8ed60402f7dcf88e435f792fdad4e0d55c437205
girino <girinovey@girino.org>
2015-07-28 00:59:31 -0300

    refactoring

--- GenericAPI.php
+++ GenericAPI.php
@@ -1,133 +1,73 @@
 <?php
 
-require_once('blinktrade_api.php');
-require_once('bitfinex_api.php');
-require_once('bitcointoyou_api.php');
+require_once('query_functions.php');
 
 abstract class Order {
-	private $currency_from;
-	private $currency_to;
+	private $currency;
+	private $order_map;
+	private $index_price;
+	private $index_amount;
+	private $index_id;
 	
-	public  function __construct($currency_from, $currency_to) {
-		$this->currency_from = $currency_from;
-		$this->currency_to = $currency_to;
+	public  function __construct($order, $currency, $index_price, $index_amount, $index_id=false) {
+		$this->currency = $currency;
+		$this->order_map = $order;
+		$this->index_price = $index_price;
+		$this->index_amount = $index_amount;
+		$this->index_id = $index_id;
 	}
 	
-	public function getCurrencyPair() {
-		return array($currency_from, $currency_to);
+	public function getCurrency() {
+		return $currency;
 	}
 	
-	public function getCurrencyFrom() {
-		return $currency_from;
+	public function getPrice($currency) {
+		$price = $this->order_map[$this->index_price];
+		if ($currency == 'BTC') return 1.0/$price;
+		return $price;
 	}
-	
-	public function getCurrencyTo() {
-		return $currency_to;
+	public function getVolume($currency) {
+		$volume = $this->order_map[$this->index_amount];
+		if ($currency == 'BTC')	return $volume;
+		return $this->order_map[$this->index_price] * $volume;
 	}
-	
+	public function getId() {
+		if ($this->index_id === false) {
+			return false;
+		}
+		return $this->order_map[$this->index_id];
+	}
 	public function getOtherCurrency($currency) {
-		if ($currency == $this->currency_from) return $this->currency_to;
-		return $this->currency_from;
+		if ($currency == 'BTC') return $this->currency;
+		return 'BTC';
 	}
-	
-	abstract public function getPrice($currency);
-	abstract public function getVolume($currency);
-	abstract public function getId();
 }
 
 class BlinktradeOrder extends Order {
-	
-	private $order_map;
-	private $price_currency = 'BRL';
-	private $volume_currency = 'BTC';
-	
-	public  function __construct($order, $currency_from = 'BTC', $currency_to = 'BRL') {
-		parent::__construct($currency_from, $currency_to);
-		$this->order_map = $order;
-	}
-	
-	public function getPrice($currency) {
-		if 	($currency == $this->price_currency) {
-			return $this->order_map[0];
-		} else {
-			return 1.0 / $this->order_map[0];
-		}
-	}
-	
-	public function getVolume($currency) {
-		if 	($currency == $this->volume_currency) {
-			return $this->order_map[1];
-		} else {
-			return $this->order_map[0] * $this->order_map[1];
-		}
-	}
-	
-	public function getId() {
-		return $this->order_map[2];
+	public  function __construct($order) {
+		parent::__construct($order, 'BRL', 0, 1, 2);
 	}
 }
-
 class BitfinexOrder extends Order {
-
-	private $order_map;
-	private $price_currency = 'USD';
-	private $volume_currency = 'BTC';
-
-	public  function __construct($order, $currency_from = 'BTC', $currency_to = 'USD') {
-		parent::__construct($currency_from, $currency_to);
-		$this->order_map = $order;
-	}
-
-	public function getPrice($currency) {
-		if 	($currency == $this->price_currency) {
-			return $this->order_map['price'];
-		} else {
-			return 1.0 / $this->order_map['price'];
-		}
-	}
-
-	public function getVolume($currency) {
-		if 	($currency == $this->volume_currency) {
-			return $this->order_map['amount'];
-		} else {
-			return $this->order_map['price'] * $this->order_map['amount'];
-		}
-	}
-
-	public function getId() {
-		return $this->order_map['timestamp'];
+	public  function __construct($order) {
+		parent::__construct($order, 'USD', 'price', 'amount', 'timestamp');
 	}
 }
 
 class B2UOrder extends Order {
-	
-	private $order_map;
-	private $price_currency = 'BRL';
-	private $volume_currency = 'BTC';
-	
-	public  function __construct($order, $currency_from = 'BTC', $currency_to = 'BRL') {
-		parent::__construct($currency_from, $currency_to);
-		$this->order_map = $order;
+	public  function __construct($order) {
+		parent::__construct($order, 'BRL', 0, 1);
 	}
-	
-	public function getPrice($currency) {
-		if 	($currency == $this->price_currency) {
-			return $this->order_map[0];
-		} else {
-			return 1.0 / $this->order_map[0];
-		}
+}
+
+class MBTCOrder extends Order {
+	public  function __construct($order) {
+		parent::__construct($order, 'BRL', 0, 1);
 	}
-	
-	public function getVolume($currency) {
-		if 	($currency == $this->volume_currency) {
-			return $this->order_map[1];
-		} else {
-			return $this->order_map[0] * $this->order_map[1];
-		}
-	}
-	
-	public function getId() {
-		return 0;
+}
+class NegocieCoinsOrder extends Order {
+	public  function __construct($order) {
+		parent::__construct($order, 'USD', 'price', 'quantity');
 	}
 }
 
@@ -209,38 +149,98 @@ class B2UOrderbook extends GenericOrderbook {
 	}
 }
 
+class MBTCOrderbook extends GenericOrderbook {
+	public function getOrderBook($operation) {
+		$orderbook = mbtc_api_query('orderbook');
+		$orders = $orderbook[$operation];
+		$ret = array();
+		foreach ($orders as $order) {
+			array_push($ret, new MBTCOrder($order));
+		}
+		return $ret;
+	}
+}
+
+class NegocieCoinsOrderbook extends GenericOrderbook {
+	private function convert_operation($operation) {
+		if ($operation == "asks") return "ask";
+		return "bid";
+	}
+	public function getOrderBook($operation) {
+		$orderbook = negociecoins_api_query('orderbook');
+		$orders = $orderbook[$this->convert_operation($operation)];
+		$ret = array();
+		foreach ($orders as $order) {
+			array_push($ret, new NegocieCoinsOrder($order));
+		}
+		return $ret;
+	}
+}
+
 abstract class FeeCalculator {
-	public function applyDepositFee($value, $currency) { return $value; }
-	public function applyWithdrawalFee($value, $currency) { return $value; }
-	public function applyExecutingOrderFee($value, $currency) { return $value; }
-	public function applyExecutedOrderFee($value, $currency) { return $value; }
+	private $fees = array();
+	public function setFee($currency, $operation, $variable, $fixed) {
+		if (!array_key_exists($currency, $this->fees)) {
+			$this->fees[$currency] = array();
+		}
+		if (!array_key_exists($operation, $this->fees[$currency])) {
+			$this->fees[$currency][$operation] = array();
+		}
+		$this->fees[$currency][$operation]['variable'] = $variable;
+		$this->fees[$currency][$operation]['fixed'] = $fixed;
+	}
+	public function getFee($currency, $operation, $type) {
+		if (!array_key_exists($currency, $this->fees)) {
+			return 0;
+		}
+		if (!array_key_exists($operation, $this->fees[$currency])) {
+			return 0;
+		}
+		if (!array_key_exists($type, $this->fees[$currency][$operation])) {
+			return 0;
+		}
+		return $this->fees[$currency][$operation][$type];
+	}
+	
+	protected function applyFee($value, $currency, $operation) {
+		$variable = $this->getFee($currency, $operation, 'variable');
+		$fixed = $this->getFee($currency, $operation, 'fixed');
+		return $value * ( 1.0 - $variable) - $fixed;
+	}
+	
+	public function applyDepositFee($value, $currency) {
+		return $this->applyFee($value, $currency, 'deposit'); 
+	}
+	public function applyWithdrawalFee($value, $currency) {
+		return $this->applyFee($value, $currency, 'withdrawal'); 
+	}
+	public function applyExecutingOrderFee($value, $currency) {
+		return $this->applyFee($value, $currency, 'executing'); 
+	}
+	public function applyExecutedOrderFee($value, $currency) {
+		return $this->applyFee($value, $currency, 'executed'); 
+	}
 }
 
 class FoxbitFeeCalculator extends FeeCalculator {
 	private $order_fee = 0.0025;
-	private $tx_fee = 0.0001;
-	private $withdrawal_fee = 0.0139;
-	public function applyWithdrawalFee($value, $currency) { 
-		if ($currency == 'BTC')
-			return $value - $this->tx_fee;
-		else 
-			return $value * (1-$this->withdrawal_fee); 
-	}
-	public function applyExecutingOrderFee($value, $currency) { 
-		return $value * (1-$this->order_fee); 
-	}
-	public function applyExecutedOrderFee($value, $currency) { 
-		return applyExecutingOrderFee($value, $currency); 
+	public  function __construct() {
+		$this->setFee('BTC', 'withdrawal', 0, 0.0001);
+		$this->setFee('BRL', 'withdrawal', 0.0139, 0);
+		$this->setFee('BTC', 'executing', $this->order_fee, 0);
+		$this->setFee('BRL', 'executing', $this->order_fee, 0);
+		$this->setFee('BTC', 'executed', $this->order_fee, 0);
+		$this->setFee('BRL', 'executed', $this->order_fee, 0);
 	}
 }
 
 class BitfinexFeeCalculator extends FeeCalculator {
 	private $order_fee = 0.002;
-	public function applyExecutingOrderFee($value, $currency) {
-		return $value * (1-$this->order_fee);
-	}
-	public function applyExecutedOrderFee($value, $currency) {
-		return applyExecutingOrderFee($value, $currency);
+	public  function __construct() {
+		$this->setFee('BTC', 'executing', $this->order_fee, 0);
+		$this->setFee('USD', 'executing', $this->order_fee, 0);
+		$this->setFee('BTC', 'executed', $this->order_fee, 0);
+		$this->setFee('USD', 'executed', $this->order_fee, 0);
 	}
 }
 
@@ -248,50 +248,87 @@ class BitfinexFeeCalculator extends FeeCalculator {
 class B2UFeeCalculator extends FeeCalculator {
 	private $order_fee = 0.0025;
 	private $executing_order_fee = 0.006;
-	private $tx_fee = 0.0001;
-	private $withdrawal_fee = 0.0189;
-	private $deposit_fee = 0.0189;
-	public function applyDepositFee($value, $currency) { 
-		if ($currency == 'BTC')
-			return $value;
-		else 
-			return $value * (1-$this->deposit_fee); 
-	}
-	public function applyWithdrawalFee($value, $currency) { 
-		if ($currency == 'BTC')
-			return $value - $this->tx_fee;
-		else 
-			return $value * (1-$this->withdrawal_fee); 
-	}
-	public function applyExecutedOrderFee($value, $currency) {
-		return $value * (1-$this->order_fee);
-	}
-	public function applyExecutingOrderFee($value, $currency) {
-		return $value * (1-$this->executing_order_fee);
+
+	public  function __construct() {
+		$this->setFee('BTC', 'deposit', 0, 0);
+		$this->setFee('BRL', 'deposit', 0.0189, 0);
+		$this->setFee('BTC', 'withdrawal', 0, 0.0001);
+		$this->setFee('BRL', 'withdrawal', 0.0189, 0);
+		$this->setFee('BTC', 'executing', $this->executing_order_fee, 0);
+		$this->setFee('BRL', 'executing', $this->executing_order_fee, 0);
+		$this->setFee('BTC', 'executed', $this->order_fee, 0);
+		$this->setFee('BRL', 'executed', $this->order_fee, 0);
 	}
 }
 
-$foxbit = array( new FoxBitOrderbook(),  new FoxbitFeeCalculator(), 'BRL', 'FOXBIT' );
+class MBTCFeeCalculator extends FeeCalculator {
+	private $order_fee = 0.003;
+	private $executing_order_fee = 0.007;
+
+	public  function __construct() {
+		$this->setFee('BTC', 'deposit', 0, 0);
+		$this->setFee('BRL', 'deposit', 0.0199, 2.90);
+		$this->setFee('BTC', 'withdrawal', 0, 0.0001);
+		$this->setFee('BRL', 'withdrawal', 0.0199, 2.90);
+		$this->setFee('BTC', 'executing', $this->executing_order_fee, 0);
+		$this->setFee('BRL', 'executing', $this->executing_order_fee, 0);
+		$this->setFee('BTC', 'executed', $this->order_fee, 0);
+		$this->setFee('BRL', 'executed', $this->order_fee, 0);
+	}
+}
+
+class NegocieCoinsFeeCalculator extends FeeCalculator {
+	private $order_fee = 0.002;
+	private $executing_order_fee = 0.003;
+
+	public  function __construct() {
+		$this->setFee('BTC', 'withdrawal', 0, 0.0001);
+		$this->setFee('BRL', 'withdrawal', 0, 7.50);
+		$this->setFee('BTC', 'executing', $this->executing_order_fee, 0);
+		$this->setFee('BRL', 'executing', $this->executing_order_fee, 0);
+		$this->setFee('BTC', 'executed', $this->order_fee, 0);
+		$this->setFee('BRL', 'executed', $this->order_fee, 0);
+	}
+}
+
+$foxbit = array( new FoxBitOrderbook(),  new FoxbitFeeCalculator(), 'BRL', 'FOX' );
 $b2u = array( new B2UOrderbook(),  new B2UFeeCalculator(), 'BRL', 'B2U' );
+$mbtc = array( new MBTCOrderbook(),  new MBTCFeeCalculator(), 'BRL', 'MBTC' );
+$negocie = array( new NegocieCoinsOrderbook(),  new NegocieCoinsFeeCalculator(), 'BRL', 'NEGOCIE' );
 $bitfinex = array( new BitFinexOrderbook(),  new BitFinexFeeCalculator(), 'USD', 'BITFINEX' );
 
-$pairs = array( array($foxbit, $bitfinex),
-		array($b2u, $bitfinex),
-		array($b2u, $foxbit),
-		array($bitfinex, $foxbit),
-		array($bitfinex, $b2u),
-		array($foxbit, $b2u),
-	);
+$brls = array($foxbit, $b2u, $mbtc, $negocie);
+$usds = array($bitfinex);
+
+$pairs = array();
+// BRL -> USD
+foreach ($brls as $brl) {
+	foreach ($usds as $usd) {
+		array_push($pairs, array($brl, $usd));
+	}
+}
+// // USD -> BRL
+// foreach ($brls as $brl) {
+// 	foreach ($usds as $usd) {
+// 		array_push($pairs, array($usd, $brl));
+// 	}
+// }
+// // BRL -> BRL
+// foreach ($brls as $brl1) {
+// 	foreach ($brls as $brl2) {
+// 		array_push($pairs, array($brl1, $brl2));
+// 	}
+// }
 
 function getValueOrders($value, $currency_pair, $book, $feecalc, $operation, $withdrawal = true) {
 	$buy = $feecalc->applyDepositFee($value, $currency_pair[0]);
 	$buy = $feecalc->applyExecutingOrderFee($buy, $currency_pair[0]);
-	//print "$buy\n";
+// 	print "-----\n$buy\n";
 	$bought = $book->getVolumeOrdered($buy, $currency_pair[0], $operation);
-	//print "$bought\n";
+// 	print "$bought\n";
 	if ($withdrawal)
 		$bought = $feecalc->applyWithdrawalFee($bought,  $currency_pair[1]);
-	//print "$bought\n";
+// 	print "$bought\n-----\n";
 	return $bought;
 }
 
@@ -303,7 +340,7 @@ foreach ($pairs as $pair) {
 	$bought = getValueOrders($value, $currency_pair_from, $book_from, $fee_from, 'asks');
 	$book_to = $pair[1][0];
 	$fee_to = $pair[1][1];
-	$currency_pair_to = array('BTC', $pair[1][2], 'BTC');
+	$currency_pair_to = array('BTC', $pair[1][2]);
 	$sold = getValueOrders($bought, $currency_pair_to, $book_to, $fee_to, 'bids', false);
 
 	$rate = $value / $sold;
--- query_functions.php
+++ query_functions.php
@@ -4,7 +4,7 @@ function api_query($url, array $req = array()) {
 
 	$post_data = http_build_query($req, '', '&');
 	$url = "$url?$post_data";
-
+	
 	// our curl handle (initialize if required)
 	static $ch = null;
 	if (is_null($ch)) {
@@ -17,11 +17,45 @@ function api_query($url, array $req = array()) {
 
 	// run the query
 	$res = curl_exec($ch);
-
+	
 	if ($res === false) throw new Exception('Could not get reply: '.curl_error($ch));
 	$dec = json_decode($res, true);
 	if (!$dec) throw new Exception('Invalid data received, please make sure connection is working and requested API exists');
 	return $dec;
+}
+
+function b2u_api_query($method, array $req = array()) {
+	$url = "http://bitcointoyou.com/API/$method.aspx";
+	return api_query($url, $req);
+}
+
+function bitfinex_api_query($currency, $method, array $req = array()) {
+
+	$api_ver = 'v1';
+	$url = "https://api.bitfinex.com/$api_ver/$method/$currency";
+
+	return api_query($url, $req);
+}
+
+function blinktrade_api_query($currency, $method, array $req = array()) {
+
+	$api_ver = 'v1';
+	$url = "https://api.blinktrade.com/api/$api_ver/$currency/$method";
+	return api_query($url, $req);
+}
+
+function foxbit_api_query($method, array $req = array()) {
+	return blinktrade_api_query("BRL", $method, $req);
+}
+
+function mbtc_api_query($method, array $req = array()) {
+	$url = "https://www.mercadobitcoin.com.br/api/$method/";
+	return api_query($url, $req);
+}
+
+function negociecoins_api_query($method, array $req = array()) {
+	$url = "http://www.negociecoins.com.br/api/v3/btcbrl/$method";
+	return api_query($url, $req);
 }
 
 ?>
